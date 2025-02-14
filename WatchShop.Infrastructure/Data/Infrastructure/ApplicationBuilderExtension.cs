174e931b2d32175b43ca3d889acb9bfefbe54c8a
Brskstdnv <bobara188@gmail.com>
2025-02-14 11:13:27 +0200

    part 3 2 stranica

---
+++ WatchShop.Infrastructure/Data/Infrastructure/ApplicationBuilderExtension.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder.Internal;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchShop.Infrastructure.Data.Infrastructure
+{
+    public static class ApplicationBuilderExtension
+    {
+        
+        public static async Task<ApplicationBuilder> PrepareDatabase(this ApplicationBuilder app)
+        {
+            using var serviceScop = app.ApplicationServices.CreateScope();
+
+            var services = serviceScop.ServiceProvider;
+
+            await RoleSeeder(services);
+            await SeedAdministrator(services);
+
+            return app;
+        }
+
+        private static async Task RoleSeeder(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            string[] roleNames = { "Administrator", "Client" };
+
+            IdentityResult roleResult;
+
+            foreach(var roleName in roleNames) 
+            {
+
+            }
+        }
+    }
+}
--- WatchShopApp.Core/obj/Debug/net6.0/WatchShopApp.Core.AssemblyInfo.cs
+++ WatchShopApp.Core/obj/Debug/net6.0/WatchShopApp.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WatchShopApp.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bcb5aa26a9231dd3945bf1ec5acb37878e14acf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WatchShopApp.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WatchShopApp.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
